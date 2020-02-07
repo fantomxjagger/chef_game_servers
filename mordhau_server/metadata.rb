2b199098f3460186c5ccbe8fff6d22b9be241df1
Zitterkopf <Jesse.Zitterkopf@nordstrom.com>
2020-02-07 16:25:44 -0700

    minor updates

--- mordhau_server/metadata.rb
+++ mordhau_server/metadata.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-name 'mordhau_server'
-maintainer 'Zach Degelau'
-maintainer_email 'theman@mordhau.com'
-license 'all_rights'
-description 'Installs/Configures mordhau_server'
-long_description 'Installs/Configures mordhau_server'
-version '0.3.26'
-issues_url 'https://github.com/learn-chef/learn_chef_httpd/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/learn-chef/learn_chef_httpd' if respond_to?(:source_url)
-depends 'yum'
-depends 'tar'
-=======
 # frozen_string_literal: true
 
 name 'mordhau_server'
@@ -19,7 +6,7 @@ maintainer_email 'theman@mordhau.com'
 license 'all_rights'
 description 'Installs/Configures mordhau_server'
 long_description 'Installs/Configures mordhau_server'
-version '0.4.4'
+version '0.4.5'
 issues_url 'https://github.com/fantomxjagger/chef_game_servers/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/fantomxjagger/chef_game_servers/tree/master/mordhau_server' if respond_to?(:source_url)
->>>>>>> 13b72ed5d5b7b4a33159ddaa63d14d6f4de86a0a
+depends 'tar'
--- mordhau_server/recipes/default.rb
+++ mordhau_server/recipes/default.rb
@@ -1,88 +1,3 @@
-<<<<<<< HEAD
-#
-# Cookbook Name:: learn_chef_httpd
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-# Firewall Rules needed - Open 7777,7778,1500 UDP
-
-# setup variables
-steam_user = node['mordhau']['steam']['user']
-steam_group = node['mordhau']['steam']['group']
-steam_shell = node['mordhau']['steam']['user_shell']
-steam_home = node['mordhau']['steam']['user_home']
-steam_tar = node['mordhau']['steam']['steam_cmd_package']
-steam_url = "#{node['mordhau']['steam']['steam_cmd_url_root']}#{steam_tar}"
-mordhau_service = node['mordhau']['steam']['server']['status']
-
-packages = node['mordhau']['os_pkgs']
-packages.each do |_pkg, details|
-  yum_package details['pkgname'] do
-    action :install
-    version details['pkgver'] if details['pkgver']
-  end
-end
-
-user steam_user do
-  comment steam_user
-  home steam_home
-  shell steam_shell
-  password 'steam'
-end
-
-
-directory steam_home do
-  owner steam_user
-  group steam_group
-  recursive true
-  mode '0755'
-  action :create_if_missing
-end
-
-# Download and extract the steamcmd.sh
-tar_extract steam_url do
-  owner steam_user
-  group steam_group
-  target_dir steam_home
-  creates "#{steam_home}/steamcmd.sh"
-end
-
-file "#{steam_home}/steamcmd.sh" do
-  owner steam_user
-  group steam_group
-  mode '0755'
-end
-
-# For Every other update, this job will run in cron once weekly or otherwise.
-execute 'FirstTime_Mordhau_Install' do
-  command './steamcmd.sh +login anonymous +runscript update_mordhau.txt'
-  cwd '/home/Steam/'
-  user 'steam'
-  group 'steam'
-  not_if { ::File.exists?('/home/Steam/mordhau/MordhauServer.sh')}
-end
-
-templates = node['mordhau']['templates']
-templates.each do |_template,details|
-  template details['target'] do
-    source details['source']
-    owner steam_user
-    group steam_group
-    mode details['mode']
-    action create_if_missing
-  end
-end
-
-# Now it actually starts the Mordhau game server
-service 'MordhauServer' do
-  start_command 'nohup ./MordhauServer.sh &'
-  stop_command 'pkill -9 MordhauServer.sh'
-  restart_command 'pkill -9 MordhauServer.sh && nohup ./MordhauServer.sh & || nohup ./MordhauServer.sh &'
-  action mordhau_service
-end
-
-include 'mordhau_server::yum_repos'
-=======
 # frozen_string_literal: true
 
 #
@@ -143,13 +58,12 @@ templates.each do |_template, details|
   end
 end
 
-# Installs SteamCMD, and Tar Cookbook would be better for this if I could get it to work
-execute 'Install SteamCMD' do
-  command "curl -sqL #{node['mordhau']['steam']['steamcmd_url']} | tar zxvf -"
-  cwd steam_home
-  user steam_user
+# Installs SteamCMD
+tar_extract steam_url do
+  owner steam_user
   group steam_group
-  not_if { ::File.exist?('/home/Steam/steamcmd.sh') }
+  target_dir steam_home
+  creates "#{steam_home}/steamcmd.sh"
 end
 
 # Creates Mordhau txt file that SteamCMD needs to install or update Mordhau
@@ -173,27 +87,17 @@ execute 'FirstTime_Mordhau_Install' do
   not_if { ::File.exist?('/home/steam/mordhau/MordhauServer.sh') }
 end
 
-# Create the Mordhau Start Script
-templates = node['mordhau']['templates']['mordhau_start_script']
-templates.each do |_template, details|
-  template details['target'] do
-    source details['source']
-    owner steam_user
-    group steam_group
-    mode details['mode']
-    action :create
-  end
-end
-
 # Runs the Mordhau Initial Start Script - First Time Run & Close to Fetch Game.ini and Engine.ini Files
-execute 'Initial_Mordhau_Run' do
-  command './initial.mordhau.run.sh'
-  cwd mordhau_root_dir
-  user steam_user
-  group steam_group
+service 'MordhauServer' do
+  start_command "nohup #{mordhau_root_dir}/MordhauServer.sh & && echo $! > /var/run/mordhau_server.pid"
+  stop_command 'kill -9 $(cat /var/run/mordhau_server.pid)&& rm /var/run/mordhau_server.pid'
+  restart_command "kill -9 $(cat /var/run/mordhau_server.pid) && rm /var/run/mordhau_server.pid && nohup #{mordhau_root_dir}/MordhauServer.sh & && echo $! > /var/run/mordhau_server.pid || nohup #{mordhau_root_dir}/MordhauServer.sh &"
+  action start
 end
+#------
 
 # Edits the Game.ini and Engine.ini files that were created by the initial mordhau start script
+
 templates = node['mordhau']['templates']['mordhau_files']
 templates.each do |_template, details|
   template details['target'] do
@@ -202,16 +106,8 @@ templates.each do |_template, details|
     group steam_group
     mode details['mode']
     action :create
+    notifies :stop, 'service[MordhauServer]', :before if ::File.exist?('/var/run/mordhau_server.pid')
+    notifies :start, 'service[MordhauServer]', :before
   end
 end
-#-----------------------------------------------------------------
-# Now it actually starts the Mordhau game server
-execute 'Start Mordhau Server' do
-  command 'nohup ./MordhauServer.sh &'
-  cwd mordhau_root_dir
-  user steam_user
-  group steam_group
-end
-
-#-----------------------------------------------------------------
->>>>>>> 13b72ed5d5b7b4a33159ddaa63d14d6f4de86a0a
+------------------------------------------------
